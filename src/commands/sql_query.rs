e2d0302707ec01cd1f004bc3e0eaacd9de343e0e
Valentyn Kit <valentynkit.work@pm.me>
2026-09-09 20:44:08 +0300

    codecrafters submit [skip ci]

---
+++ src/commands/sql_query.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+
+use anyhow::bail;
+use tracing::{debug, info};
+
+use crate::commands::helpers::{DbHeader, SqliteSchema, page_header, parse_cell, read_page};
+
+pub(crate) fn run(
+    file: &File,
+    schemas: Vec<SqliteSchema>,
+    db_hdr: DbHeader,
+    query: Vec<String>,
+) -> anyhow::Result<String> {
+    assert_eq!(query.len(), 1, "query should be one element");
+    assert!(
+        schemas.len() > 1,
+        "schemas expected to have more than 1 item"
+    );
+    info!("executing command {query:?}, query_len {}", query.len());
+    let tokens = query[0].split(' ').collect::<Vec<&str>>();
+    let second_arg = tokens
+        .get(1)
+        .expect("query is expected to have second element");
+
+    let from_table = tokens
+        .last()
+        .expect("query should have the last item after FROM available");
+
+    let Some(item) = schemas.iter().find(|&item| item.tbl_name() == *from_table) else {
+        bail!("could find table with tbl_name: {from_table}");
+    };
+
+    debug!(?item);
+
+    let page_size = db_hdr.page_size();
+    let mut page_buf = vec![0u8; page_size as usize];
+    read_page(&file, &mut page_buf, page_size, item.rootpage_index())?;
+
+    let page_header = page_header(&page_buf, item.rootpage_index())?;
+
+    for &ptr in page_header.cell_pointers() {
+        debug!(offset = ptr, "start parsing cell");
+        let (_cell, _) = parse_cell(&page_buf[(ptr as usize)..])?;
+    }
+    let out = if *second_arg == "COUNT(*)" {
+        format!("{}", page_header.cell_count())
+    } else {
+        "Uknown second argument".into()
+    };
+
+    Ok(out)
+}
